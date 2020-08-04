62c3f7e748207357137c69c108c06781c9cb55bc
Amanda Yonce <amanda.stork@gmail.com>
2020-08-04 13:16:59 -0400

    final project

--- hello_app/views.py
+++ hello_app/views.py
@@ -3,5 +3,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def index_view(request):
-    # context = {'text': "Hello World"}
     return render(request, 'index.html', {'text': "Hello World"})
